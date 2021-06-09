18a35851419d69c8c9a53b226334a6c403741392
philippe-blanquier <philippe.blanquier@gmail.com>
2021-06-09 15:27:38 +0200

    creation

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang='fr'>
+	<head>
+        <meta charset='UTF-8'>
+        <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+        <title>PHP ex 3.3 boucle</title>
+    </head>
+    <body >
+        <p>
+            <?php
+            $v1=100;
+            $v2=1; // 1..100
+            while ($v1 > 10)
+            {
+               echo nl2br( $v1.'*'.$v2.'='.$v1*$v2."\n");
+               $v1--; 
+            }
+            ?>
+       </p>
+    </body>
+</html>
